9c5adad8ed0707e8fbbc65f0f8b0732272f87673
vilkencalle <125492342+vilkencalle@users.noreply.github.com>
2025-04-10 22:33:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function calculateIBU() {
     const time = parseFloat(row.children[3].value);   // min
 
     if (!isNaN(weight) && !isNaN(alpha) && !isNaN(time)) {
-      const (utilization = 1.65 * Math.pow(0.000125, og - 1) * (1 - Math.exp(-0.04 * time)) / 4.15)*1.15;
+      const utilization = (1.65 * Math.pow(0.000125, og - 1) * (1 - Math.exp(-0.04 * time)) / 4.15)*1.15;
       const ibu = (weight * alpha * utilization * 10) / batchSize;
       ibuTotal += ibu;
     }
